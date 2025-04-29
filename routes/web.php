dde731eb1fd5a4756009b964bda38a7463e4b166
John Bryan T. Palabay <124575239+JeybPalabay@users.noreply.github.com>
2025-04-29 21:23:40 +0800

    Product and Category

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminController;
 
 route::get('/',[HomeController::class, 'index']);
 
@@ -9,10 +10,17 @@ Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
-])->group(function () {
+])->group(function () { 
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
 });
 Route::get('/redirect', [HomeController::class, 'redirect']);
+
+Route::get('/view_category', [AdminController::class, 'view_category']);
+Route::post('/add_category', [AdminController::class, 'add_category']);
+Route::get('/delete_category/{id}', [AdminController::class, 'delete_category']);
+Route::get('/view_product', [AdminController::class, 'view_product']);
+
+
 
