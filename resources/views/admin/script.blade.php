288fb7ee8b28cb00859ae9043f0ae5d0dc3593a3
JeybPalabay <johnbryanpalabay2@gmail.com>
2025-05-03 00:29:05 +0800

    fix debug

--- resources/views/admin/script.blade.php
+++ resources/views/admin/script.blade.php
@@ -16,7 +16,5 @@
     <script src="admin/assets/js/todolist.js"></script>
     <!-- endinject -->
     <!-- Custom js for this page -->
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.6.2/js/bootstrap.min.js"></script>
     <script src="admin/assets/js/dashboard.js"></script>
     <!-- End custom js for this page -->
